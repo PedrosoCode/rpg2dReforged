5fcd81a8cd12a8d052b772f4f92ace350b4867b7
Gabriel <Gabrielpedrofc@gmail.com>
2024-02-17 00:05:47 -0300

    first commit

---
+++ includes/navbar.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="#">Página 1</a></li>
+        <li><a href="#">Página 2</a></li>
+        <li><a href="#">Página 3</a></li>
+        <li><a href="#">Página 4</a></li>
+    </ul>
+</nav>
